8cf1c164504379a0461a17f3acddd899948bd141
Shelby_mann <maandalsania11@gmail.com>
2023-03-06 12:56:34 +0530

    new

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+var slider ;
+var angle = PI/4;
+var rotation;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1200, 700);
+    slider = createSlider(0, TWO_PI, PI/4,0.01)
+    
   }
   
+
 function draw() {
-    background(220);
+    stroke(255)
+    background(51);
+    var len = 200;
+    translate(width/2, height)
+    // line(0,0, 0, -100)
+    
+    angle = slider.value()
+    branch(len)
   }
+function branch(len){
+    line(0,0,0,-len);
+    translate(0,-len);
+    
+
+    if(len>4){
+        push();
+        rotate(-angle);
+        branch(len*0.67);
+        pop();
+        push();
+        rotate(angle)
+        branch(len*0.67);
+        pop();
+    }
+    
+}
